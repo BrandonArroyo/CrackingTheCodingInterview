71fbb333b39692282e8618c75940f255643bfbc2
BrandonArroyo <barroyo50@gmail.com>
2015-09-24 13:36:38 -0500

    ∆ isunique to make it pretty

--- Chapter1/1.1Python/isUnique.py
+++ Chapter1/1.1Python/isUnique.py
@@ -11,28 +11,3 @@ def isUnique(s):
         else:
             array[ord(i)] = i
     return True
-#Unit tests
-
-# def tests():
-#     red = '\033[91m'
-#     close =  '\033[0m'
-#     green = '\033[92m'
-#
-#     if isUnique("abcd"):
-#         print "Case 1: " + green + "Pass"+ close
-#     else:
-#         print "Case 1:"+red+ "Fail" + close
-#     if isUnique("aabbccdd"):
-#         print "Case 2:"+red+ "Fail" + close
-#     else:
-#         print "Case 2: " + green + "Pass"+ close
-#     if isUnique("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"):
-#         print "Case 3: " + green + "Pass"+ close
-#     else:
-#         print "Case 3:"+ red+ "Fail" + close
-#     if isUnique("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890a"):
-#         print "Case 4:"+red+ "Fail" + close
-#     else:
-#         print "case 4: " + green + "Pass"+ close
-#
-# tests()
