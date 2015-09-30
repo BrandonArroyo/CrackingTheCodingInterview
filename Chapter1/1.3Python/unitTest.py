87d868f365a7f1b6bc61ec9abf8fb566063268b5
BrandonArroyo <barroyo50@gmail.com>
2015-09-29 23:27:03 -0500

    + chapter 2 stuff

--- Chapter1/1.3Python/unitTest.py
+++ Chapter1/1.3Python/unitTest.py
@@ -4,19 +4,19 @@ def tests():
     close =  '\033[0m'
     green = '\033[92m'
 
-    if  urlIfy.change("abcd") == "abcd":
+    if  urlIfy.change("abcd  ",7) == "abcd%20":# insert in back
         print "Case 1: " + green + "Pass"+ close
     else:
         print "Case 1:"+red+ "Fail" + close
-    if urlIfy.change("aabbccdd") == "ads":
+    if urlIfy.change("a a bb ccd      ",10) == "a%20a%20bb%20ccd": #multiple insert middle
         print "Case 2:"+red+ "Fail" + close
     else:
         print "Case 2: " + green + "Pass"+ close
-    if  urlIfy.change("john") == "john":
+    if  urlIfy.change("john",4) == "john": #no insertion
         print "Case 3: " + green + "Pass"+ close
     else:
         print "Case 3:"+ red+ "Fail" + close
-    if  urlIfy.change("a") == "bob":
+    if  urlIfy.change(" hello  ",6) == "%20hello": #insert front
         print "Case 4:"+red+ "Fail" + close
     else:
         print "case 4: " + green + "Pass"+ close
--- Chapter1/1.3Python/urlIfy.py
+++ Chapter1/1.3Python/urlIfy.py
@@ -1,3 +1,7 @@
 # return a string for the unit test
 def change(str, size):
-    return "tset"
+    #can break the string apart into substrings then put them back together
+    #loop through the string until find " " then do something
+    while s < size:
+        
+    return str
