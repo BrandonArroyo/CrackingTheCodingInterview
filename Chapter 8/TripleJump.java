149fac38de5a42ec157932953cd0f1bcd6e604bc
BrandonArroyo <barroyo50@gmail.com>
2015-10-02 18:47:46 -0500

    + push

--- Chapter 8/TripleJump.java
+++ Chapter 8/TripleJump.java
@@ -3,14 +3,12 @@ public class TripleJump{
     //something to keep track of the steps taken
 
     public static int  TripleJump(int n){ // n being the number of stairs
-        //1 step
-        //Base cases
-        if(n < 0 )
+        // path so count the bottom nodes
+        if(n < 0)
             return 0;
-        else if ( n == 0)
+        if(n == 1)
             return 1;
-        else
-            return TripleJump(n-1) +TripleJump(n-2) +TripleJump(n-3);
+        return TripleJump(n -1) + TripleJump(n-2)+TripleJump](n-3);
     }
 
 
