fdedb71d7df06c1bdf8a372bd35a2230dbb035e3
BrandonArroyo <barroyo50@gmail.com>
2015-10-10 11:19:04 -0500

    +stuff

--- Chapter1/1.4Palindrome/PalindromePermutation.java
+++ Chapter1/1.4Palindrome/PalindromePermutation.java
@@ -1,47 +1,51 @@
 import java.util.*;
-
+import java.lang.*;
 public class PalindromePermutation{
-     static ArrayList<String> perms = new ArrayList<String>() ;
-     static ArrayList<String> permsNew = new ArrayList<String>() ;
 
-     public static void permutation(String str) {
-      permutation("", str);
-  }
-
-  private static void permutation(String prefix, String str) {
-      int n = str.length();
-      if (n == 0)
-          perms.add(prefix);
-      else {
-          for (int i = 0; i < n; i++){
-
-              permutation(prefix + str.charAt(i), str.substring(0, i) + str.substring(i+1, n));
-          }
-      }
-  }
-  public static boolean checkPalin(String s){
-      for(int j = 0; j < s.length()  ; ++j){
-        //   System.out.println(s);
-        //     System.out.println("s: "+s.charAt(j));
-        //     System.out.println("s2: "+s.charAt(s.length() -1-j));
-          if((s.charAt(j) != s.charAt(s.length() -1-j))){
-              return false;
-          }
-
+    public static boolean isPermutationOfPalindrome(String phrase){
+        int[] table = buildCharFrequencyTable(phrase);
+        return checkMaxOneOdd(table);
     }
-        return true;
-
-  }
-
-    public static void main(String[] args) {
-        permutation("tet");
-        for(int i = 0; i < perms.size(); ++i){
-            if(checkPalin(perms.get(i))){
-            
-                permsNew.add(perms.get(i));
+    public static boolean checkMaxOneOdd(int[] table){
+        boolean foundOdd = false;
+        for(int count: table){
+            if(count % 2 == 1){
+                if(foundOdd){
+                    return false;
+                }
+                foundOdd = true;
             }
         }
-        for(int i = 0 ; i < permsNew.size();i++)
-            System.out.println(permsNew.get(i));
+        return true;
     }
+    public static int getCharNumber(Character c){
+        int a = Character.getNumericValue('a');
+        int z = Character.getNumericValue('z');
+        int A = Character.getNumericValue('A');
+        int Z = Character.getNumericValue('Z');
+        int val = Character.getNumericValue(c);
+
+        if(a <= val && val <= z)
+            return val -A;
+        return -1;
+    }
+    public static int[] buildCharFrequencyTable(String phrase){
+        int[] table = new int[Character.getNumericValue('z')-Character.getNumericValue('a')+1];
+        for(char c : phrase.toCharArray()){
+                int x = getCharNumber(c);
+                if(x != -1){
+                    table[x]++;
+                }
+        }
+        return table;
+    }
+    public static void main(String[] args) {
+
+        System.out.println(isPermutationOfPalindrome("tutat"));
+
+
+    }
+
+
+
 }
